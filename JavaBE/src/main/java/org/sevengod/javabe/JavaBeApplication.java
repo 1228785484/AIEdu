cebe95b2953df981537522101273fc48d5c9a26e
SevenGod <1228785484@qq.com>
2024-11-17 21:02:22 +0800

    Java后端初步导入

---
+++ JavaBE/src/main/java/org/sevengod/javabe/JavaBeApplication.java
@@ -0,0 +1,13 @@
+package org.sevengod.javabe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaBeApplication.class, args);
+    }
+
+}
---
+++ JavaBE/src/test/java/org/sevengod/javabe/JavaBeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sevengod.javabe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
