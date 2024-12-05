7d0fac40ce8c5dcac376e57cc1438736a25ca89f
SevenGod <1228785484@qq.com>
2024-12-05 10:22:27 +0800

    新增了多线程处理

--- JavaBE/src/main/java/org/sevengod/javabe/config/AsyncConfig.java
+++ JavaBE/src/main/java/org/sevengod/javabe/config/AsyncConfig.java
@@ -13,12 +13,12 @@ public class AsyncConfig {
     @Bean(name = "taskExecutor")
     public Executor taskExecutor() {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-        // 8核16线程的CPU，设置核心线程数为 9
-        executor.setCorePoolSize(9);
-        // 最大线程数设置为 16，对应CPU的逻辑处理器数
-        executor.setMaxPoolSize(16);
-        // 由于是IO密集型任务，队列容量可以适当增加
-        executor.setQueueCapacity(200);
+        // 核心线程数设置为 CPU 核心数
+        executor.setCorePoolSize(8);
+        // 最大线程数设置为 CPU 逻辑处理器数
+        executor.setMaxPoolSize(12);
+        // 队列容量
+        executor.setQueueCapacity(100);
         executor.setThreadNamePrefix("DifyThread-");
         // 设置拒绝策略
         executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
