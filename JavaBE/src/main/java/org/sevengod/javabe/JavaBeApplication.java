bd19c05c65869b3db0fac18e677468b23f08b2d3
SevenGod <1228785484@qq.com>
2024-12-08 19:50:46 +0800

    修改了用Redis缓存的功能

--- JavaBE/src/main/java/org/sevengod/javabe/JavaBeApplication.java
+++ JavaBE/src/main/java/org/sevengod/javabe/JavaBeApplication.java
@@ -3,6 +3,7 @@ package org.sevengod.javabe;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
@@ -11,6 +12,7 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 @EnableAsync
 @EnableScheduling  // 启用定时任务
 @EnableAspectJAutoProxy // 启用AOP
+@EnableCaching //启动缓存
 // 确保扫描到所有需要的包
 public class JavaBeApplication {
 
