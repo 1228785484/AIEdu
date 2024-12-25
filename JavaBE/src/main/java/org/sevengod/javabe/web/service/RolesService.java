36b23cee2e08deb15a79eb187045cf97b1795aff
SevenGod <1228785484@qq.com>
2024-12-25 10:31:06 +0800

    修复重复角色bug

--- JavaBE/src/main/java/org/sevengod/javabe/web/service/RolesService.java
+++ JavaBE/src/main/java/org/sevengod/javabe/web/service/RolesService.java
@@ -6,4 +6,5 @@ import org.sevengod.javabe.entity.dto.Roles;
 public interface RolesService extends IService<Roles> {
     boolean assignTeacherRole(Long userId);
     boolean assignStudentRole(Long userId);
+    boolean checkRoleExists(Long userId);
 }
--- JavaBE/src/main/java/org/sevengod/javabe/web/service/impl/RolesServiceImpl.java
+++ JavaBE/src/main/java/org/sevengod/javabe/web/service/impl/RolesServiceImpl.java
@@ -1,5 +1,6 @@
 package org.sevengod.javabe.web.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.sevengod.javabe.entity.dto.Roles;
 import org.sevengod.javabe.web.mapper.RolesMapper;
@@ -12,19 +13,46 @@ import java.time.LocalDateTime;
 public class RolesServiceImpl extends ServiceImpl<RolesMapper, Roles> implements RolesService {
     @Override
     public boolean assignTeacherRole(Long userId) {
-        Roles teacherRole = new Roles();
-        teacherRole.setUserId(userId);
-        teacherRole.setRoleName("teacher");
-        teacherRole.setAssignedAt(LocalDateTime.now());
-        return this.save(teacherRole);
+        if (checkRoleExists(userId)) {
+            // Update existing role
+            Roles teacherRole = this.getOne(new LambdaQueryWrapper<Roles>()
+                    .eq(Roles::getUserId, userId));
+            teacherRole.setRoleName("teacher");
+            teacherRole.setAssignedAt(LocalDateTime.now());
+            return this.updateById(teacherRole);
+        } else {
+            // Create new role
+            Roles teacherRole = new Roles();
+            teacherRole.setUserId(userId);
+            teacherRole.setRoleName("teacher");
+            teacherRole.setAssignedAt(LocalDateTime.now());
+            return this.save(teacherRole);
+        }
     }
 
     @Override
     public boolean assignStudentRole(Long userId) {
-        Roles studentRole = new Roles();
-        studentRole.setUserId(userId);
-        studentRole.setRoleName("student");
-        studentRole.setAssignedAt(LocalDateTime.now());
-        return this.save(studentRole);
+        if (checkRoleExists(userId)) {
+            // Update existing role
+            Roles studentRole = this.getOne(new LambdaQueryWrapper<Roles>()
+                    .eq(Roles::getUserId, userId));
+            studentRole.setRoleName("student");
+            studentRole.setAssignedAt(LocalDateTime.now());
+            return this.updateById(studentRole);
+        } else {
+            // Create new role
+            Roles studentRole = new Roles();
+            studentRole.setUserId(userId);
+            studentRole.setRoleName("student");
+            studentRole.setAssignedAt(LocalDateTime.now());
+            return this.save(studentRole);
+        }
+    }
+
+    @Override
+    public boolean checkRoleExists(Long userId) {
+        LambdaQueryWrapper<Roles> wrapper = new LambdaQueryWrapper<>();
+        wrapper.eq(Roles::getUserId, userId);
+        return this.exists(wrapper);
     }
 }
